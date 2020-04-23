a33a6e4811ed31e6e36b315af3cf737ba75f9b65
haoyue.wang <843655240@qq.com>
2020-04-23 14:51:10 +0800

    状态更新一定是异步的问题

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ### 安装
   - 安装taro开发工具 @taro/cli yarn global add @taro/cli 
   - 使用：taro init myTaro
+### 生命周期 state
+  - 状态更新一定是异步的
+  - react 的状态更新不一定是异步的，更新数据必须使用setState方法，直接赋值不会更新组件
+  - 
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -33,7 +33,7 @@ componentDidUpdate () {
 shouldComponentUpdate (nextProps, nextState) {
   //nextProps:父组件传递子组件的参数，
   //nextState:state下一次的state，
-  // 一般多次调用时，提升render性能
+  // 一般多次调用setState时，提升render性能
 if(nextState.name == 'hell') {
   return true
 }else {
@@ -42,13 +42,22 @@ if(nextState.name == 'hell') {
   // 检查此次setState是否要进行render 调用
   console.log('state数据更新之后')
   // 判断状态时使用，
-  return false
+  // return false
 
+}
+componentWillReceiveProps () {
+  // nextProps: 下一次的props
+  // 会在父组件传递给子组件的参数发生改变时触发
 }
   componentDidShow () { 
     // 在小程序中支持
     console.log('页面显示是触发')
-    this.setState({name: 'hell'})
+    // setState有两个参数，一个是我们的state，一个是回调函数，在回调里面可以立刻拿到刚才更新的值
+    this.setState({name: 'hell'}, () => {
+      console.log(this.state.name)
+    })
+    // console.log(this.state.name) // 还是之前的值
+
   }
 
   componentDidHide () { 
