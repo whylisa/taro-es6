da9ebf8e030c81f9ed5de3d1fb23cd6cd4081e91
haoyue.wang <843655240@qq.com>
2020-04-24 13:47:30 +0800

    api路径

---
+++ blog/src/constants/api.js
@@ -0,0 +1,11 @@
+const rootPath = 'https://cnodejs.org/api.v1'
+const apiObject = {
+    getTopics: rootPath + '/topocs',// 获取话题列表
+    getTopicsInfo: rootPath + '/topic', // 获取话题详情
+    checkUserToken: rootPath + '/accesstoken', // 获取用户token
+    getUserInfo: rootPath + '/user/', // 获取用户信息
+    createTopic: rootPath + '/topics', // 新建话题
+    replyTopic: rootPath + '/topic', // 回复话题消息
+    upReply: rootPath + '/reply', // 点赞
+}
+export default apiObject
--- blog/src/pages/index/index.js
+++ blog/src/pages/index/index.js
@@ -1,25 +1,9 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
-import { connect } from '@tarojs/redux'
-
-import { add, minus, asyncAdd } from '../../actions/counter'
-
 import './index.less'
 
 
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
+
 class Index extends Component {
 
     config = {
@@ -39,10 +23,6 @@ class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
         <View><Text>Hello, World</Text></View>
       </View>
     )
